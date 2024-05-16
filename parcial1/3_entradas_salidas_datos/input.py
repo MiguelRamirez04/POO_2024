49e486211ff6ab2d38df474bc15df23ceeec2499
MiguelRamirez04 <miguelramirez.programador04@gmail.com>
2024-05-16 00:25:33 -0600

    .

---
+++ parcial1/3_entradas_salidas_datos/input.py
@@ -0,0 +1,15 @@
+"""
+En python en modo consola para la salida de datos o impresion en pantalla
+se utiliza la funcion "print"
+y para la entrada de datos se utiliza la funcion "input"
+"""
+
+# Solicitar los datos alumno
+
+nombre = input("Nombre del alumno: ")
+matricula = input("Matricula: ")
+especialidad = input("Especialidad: ")
+edad = input("Edad: ")
+estatura = input("Estatura: ")
+
+print(f"\t... ::: Datos del alumno ::: ...\n Nombre: {nombre} \n Matricula: {matricula} \n Especialidad: {especialidad} \n Edad: {edad} Estatura: {estatura}")
---
+++ parcial1/4_estructuras_control_condicional/if.py
@@ -0,0 +1,50 @@
+"""
+Existe una estructura de condicion llamada "if"
+La cual evalua una condicion pata encontrar el valor "true" o se cumple
+la condicion se ejecuta la linea o lineas de codigo
+Tienes 4 variantes del if
+
+1.- if simple
+2.- if compuesto
+3.- if anidado
+4.- if y elif
+
+"""
+
+# ejemplo 1. if simple
+
+color = "rojo"
+if color == "rojo":
+    print("soy el color rojo")
+
+# ejemplo 2. if compuesto
+
+color = "rojo"
+if color == "rojo":
+    print("soy el color rojo")
+else:
+    print("no soy del color rojo")
+
+#Ejemplo 3 if anidado
+
+color="rojo"
+if color=="rojo":
+    print("Soy el color rojo")  
+    if color!="rojo":
+       print("Soy otro color")   
+else:      
+    print("No soy el color rojo")
+    if color!="rojo":
+       print("Soy otro color") 
+
+#Ejemplo 4 if con elif
+
+color="rojo"
+if color=="rojo":
+    print("Soy el color rojo")  
+elif color=="negro":    
+    print(" soy el color negro")
+elif color=="verde":    
+    print(" soy el color verde")   
+else:
+    print("No soy rojo, verde o negro")
