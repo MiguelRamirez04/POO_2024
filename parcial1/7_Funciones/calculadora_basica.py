4a8471c6263e5da543a05c2959c81e240abc031c
MiguelRamirez04 <miguelramirez.programador04@gmail.com>
2024-06-03 09:25:18 -0600

    .

--- parcial1/7_Funciones/calculadora_basica.py
+++ parcial1/7_Funciones/calculadora_basica.py
@@ -1,24 +1,26 @@
-def solicitar_datos():
-    print("\n")
-    global n1,n2,ope
-    n1 = int(input("Numero #1: "))
-    n2 = int(input("Numero #2: "))
-    ope = input("Operacion: ").upper()
+def solicitarDatos():
+   print("\n")
+   global n1,n2,ope
+   n1=int(input("Numero #1: "))
+   n2=int(input("Numero #2: "))
+   ope=input("Operacion: ").upper()
+  
 
-
-def getCalculadora(num1, num2, operacion):
-    if operacion == "1" or operacion == "+" or operacion == "SUMA":
-        resultado = (f"{num1} + {num2} = {int(num1)+int(num2)}")
-    elif operacion == "2" or operacion == "-" or operacion == "RESTA":
-        resultado = (f"{num1} - {num2} = {int(num1)+int(num2)}")
-    elif operacion == "3" or operacion == "*" or operacion == "MULTIPLICACION":
-        resultado = (f"{num1} * {num2} = {int(num1)+int(num2)}")
-    elif operacion == "4" or operacion == "/" or operacion == "DIVISION":
-        resultado = (f"{num1} / {num2} = {int(num1)+int(num2)}")
+def getCalculadora(num1,num2,operacion):
+    if operacion=="1" or operacion=="+" or operacion=="SUMA":
+        resultado=f"{num1}+{num2}={int(num1)+int(num2)}"
+    elif operacion=="2" or operacion=="-" or operacion=="RESTA":
+        resultado=f"{num1}-{num2}={int(num1)-int(num2)}"  
+    elif operacion=="3" or operacion=="*" or operacion=="MULTIPLICACION":
+        resultado=f"{num1}*{num2}={int(num1)*int(num2)}"        
+    elif operacion=="4" or operacion=="/" or operacion=="DIVISION":
+        resultado=f"{num1}/{num2}={int(num1)/int(num2)}"      
     return resultado
 
-print("\n...::: CALCULADORA BASICA :::...\n1.- Suma\n2.- Resta\n3.- Multiplicacion \n4.- Division \n5.- Salir")
+
+print("\n\t..::: CALCULADORA BÁSICA :::... \n 1.- Suma \n 2.- Resta \n 3.-Multiplicacion \n 4.- División \n 5.- SALIR ")
+opcion=input("\t Elige una opción: ").upper()
 
 
-solicitar_datos()
+solicitarDatos()
 print(getCalculadora(n1,n2,ope))
