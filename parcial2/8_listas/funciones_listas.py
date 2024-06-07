f2905a296d0bfe5d4be52745bc0044eabbb1c778
MiguelRamirez04 <miguelramirez.programador04@gmail.com>
2024-06-07 08:53:33 -0600

    parcial2

--- parcial2/8_listas/funciones_listas.py
+++ parcial2/8_listas/funciones_listas.py
@@ -11,6 +11,37 @@ print(numeros)
 numeros.sort()
 print(numeros)
 
+
+#Añadir elementos
 print(texto)
-texto.sort()
+texto.insert(len(texto), "True")
 print(texto)
+texto.insert(len(texto),True)
+print(texto)
+texto.append(False)
+print(texto)
+
+# Dar la vuelta a una lista
+
+print(numeros)
+numeros.reverse()
+print(numeros)
+
+# buscar datos dentro de una lista
+respuesta= "Brasil" in paises
+print(respuesta)
+
+# cuantas veces aparece un valor dentro de una lista 
+print(numeros)
+numeros.append(23)
+cuantos=numeros.count(23)
+print(f"El numero 23 se encontró: {cuantos}")
+
+# unir listas
+print(paises)
+paises.extend(numeros)
+print(paises)
+
+print(numeros)
+numeros.sort()
+print(numeros)
