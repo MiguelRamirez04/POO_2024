a50a61eb3d0d33ee3ea2d15271ff2ed4ac5cd0bb
MiguelRamirez04 <146059816+MiguelRamirez04@users.noreply.github.com>
2024-06-13 08:27:42 -0600

    Delete parcial1/13_Ejercicios_Repaso_7al12 directory

--- parcial1/13_Ejercicios_Repaso_7al12/ejercicio3.py
+++
@@ -1,8 +0,0 @@
-lista =[]
-if len(lista) == 0:
-    print("la lista esta vacia, llenarla con palabras o frases")
-    while True:
-        palabra = input("Ingrese una palabra o frase(Esribir fin para dejar de agregar palabras): ").upper()
-        lista.append(palabra)
-        if palabra == "FIN":
-            break
