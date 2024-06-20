1949d322f6ab3fff53890632ceea64c053f76b30
MiguelRamirez04 <miguelramirez.programador04@gmail.com>
2024-06-20 08:53:29 -0600

    .

--- P1_Fundamentos_python/6_Ejercicios_repaso_1al5/ejercicio_8.py
+++
@@ -1,7 +0,0 @@
-# 8.- Hacer un programa que resuelva lo siguiente. ¿Cuanto es el X por ciento de X numero?
-
-porcent = int(input("Introduce el porcentaje: "))
-num = int(input("Introduce el número: "))
-
-result = (porcent / 100) * num
-print(f"El {porcent}% de {num} es: {result}")
--- P1_Fundamentos_python/6_Ejercicios_repaso_1al5/ejercicio_9.py
+++
@@ -1,8 +0,0 @@
-# 9.- Hacer un programa que solicite numeros indefinidamente hasta que se introduzca el 111 y salir del programa
-
-num = 0
-while num != 111:
-    num = int(input("Introduce un número (111 para finalizar): "))
-    if num == 111:
-        print("Programa cerrado.")
-        
